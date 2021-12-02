13d353456ff9aa9612834c52976b8dca76ebb366
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-03 02:04:18 +0300

    Made chatbox component better styled & fixed some bugs

--- Client/components/widgets/chatbox/chatbox.vue
+++ Client/components/widgets/chatbox/chatbox.vue
@@ -15,11 +15,18 @@
 <template>
   <div class="position-relative w-100 min-w-100 max-w-100 d-flex flex-row align-items-start justify-content-start overflow-hidden widget-chatbox-container">
     <img class="disable-selection owner-avatar" src="https://hi-static.z-dn.net/files/d60/c746efb8807770ea7ad2af25ee7ed2ab.jpg">
-    <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-items-start message-container">
-      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus sit dolores minima totam quis facere voluptatibus nihil, culpa modi possimus sapiente, impedit quos tempore. Beatae maxime nobis placeat temporibus ducimus?</div>
-      <div>Testing...</div>
-      <div>Testing...</div>
-      <div>Testing...</div>
+    <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-items-start">
+      <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-row disable-selection message-header">
+        <div class="owner-tag">Aviril</div>
+        <div class="tag-header">Admin</div>
+        <div class="timestamp-tag">4:50 PM</div>
+      </div>
+      <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-items-start message-body">
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus sit dolores minima totam quis facere voluptatibus nihil, culpa modi possimus sapiente, impedit quos tempore. Beatae maxime nobis placeat temporibus ducimus?</div>
+        <div>Testing...</div>
+        <div>Testing...</div>
+        <div>Testing...</div>
+      </div>
     </div>
   </div>
 </template>
