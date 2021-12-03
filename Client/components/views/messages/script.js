7fa0aca9e2c8949de8d21f93e355a8a2ea849a68
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-04 02:34:04 +0300

    Integrated personalGroupSelection

--- Client/components/views/messages/script.js
+++ Client/components/views/messages/script.js
@@ -1,19 +1,24 @@
 export default {
   data() {
-    return {}
+    return {
+      selections: {
+        personalGroup: false
+      }
+    }
   },
 
   mounted() {},
 
   computed: {
     personalGroups() {
+      const initialGroup = Object.entries(this.$store.state.groups.personal.personalGroups)[0]
+      this.selections.personalGroup = (this.selections.personalGroup && this.$store.state.groups.personal.personalGroups[(this.selections.personalGroup)]) || (initialGroup && initialGroup[0]) || false
       return this.$store.state.groups.personal.personalGroups || false
     }
   },
 
   methods: {
     getParticipantDatas(participantUID) {
-      console.log(participantUID)
       if (this.$store.state.views.contacts.userContacts["friends"] && this.$store.state.views.contacts.userContacts["friends"][participantUID]) {
         return this.$store.state.views.contacts.userContacts["friends"][participantUID]
       }
