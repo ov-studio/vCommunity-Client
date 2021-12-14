fb13aa1fae4ca49699c9c68746520e15d05aa08a
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-14 18:00:06 +0300

    Binded user online status

--- Client/components/widgets/contactbar/contactbar.vue
+++ Client/components/widgets/contactbar/contactbar.vue
@@ -14,7 +14,10 @@
 
 <template>
   <div class="position-relative w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-start overflow-hidden disable-selection widget-contactbar-container">
-    <img class="disable-selection contact-avatar" :src="contactAvatar">
+    <span class="position-relative">
+      <img class="disable-selection contact-avatar" :src="contactAvatar">
+      <span class="position-absolute contact-online-status" :isOnline="isContactOnline(contactUID)"/>
+    </span>
     <div class="overflow-hidden contact-name">{{getContactName(contactUID)}}</div>
     <div v-for="(optionData, optionIndex) in contactOptions" :key="optionIndex" class="contact-option" @click.prevent="contactOptionHandler(contactUID, optionData)">{{optionData.type}}</div>
   </div>
