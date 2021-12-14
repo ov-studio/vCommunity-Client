0972eca23c075cee910f9bdf1b2907dfcfee6344
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-14 17:21:41 +0300

    Update script.js

--- Client/components/widgets/contactbar/script.js
+++ Client/components/widgets/contactbar/script.js
@@ -2,6 +2,10 @@ export default {
   props: ["contactUID", "contactAvatar", "contactOptions", "contactOptionHandler"],
   
   methods: {
+    isContactOnline(contactUID) {
+      return this.$store.getters["users/getUserData"](contactUID, "isOnline")
+    },
+
     getContactName(contactUID) {
       return this.$store.getters["users/getUserData"](contactUID, "username") || contactUID
     }
