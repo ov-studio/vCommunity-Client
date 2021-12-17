31a9db4e91b6d40615177fb49444fbeed5d53697
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-17 21:08:00 +0300

    Update groupbar.vue

--- Client/components/widgets/groupbar/groupbar.vue
+++ Client/components/widgets/groupbar/groupbar.vue
@@ -14,7 +14,7 @@
 
 <template>
   <div class="position-relative w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-self-start align-items-center justify-content-start overflow-hidden disable-selection widget-groupbar-container">
-    <div class="groupbar-header">{{containerHeader}}</div>
+    <div class="w-100 min-w-100 max-w-100 groupbar-header">{{containerHeader}}</div>
     <div v-if="!isServerSelected" class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller contacts-container">
       <widget-contactbar v-for="(personalGroup, groupUID) in personalGroups" :key="groupUID" :contactUID="personalGroup.participantUID" contactAvatar="https://hi-static.z-dn.net/files/d60/c746efb8807770ea7ad2af25ee7ed2ab.jpg" :isSelected="isGroupSelected(groupUID)" @click.native="onClientSelectGroup(groupUID)"/>
     </div>
