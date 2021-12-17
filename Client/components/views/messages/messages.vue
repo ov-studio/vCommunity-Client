a75b74bda744611e208b86645425ee7953f53c1a
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-17 20:55:38 +0300

    Splitting into `groupbar`

--- Client/components/views/messages/messages.vue
+++ Client/components/views/messages/messages.vue
@@ -16,14 +16,7 @@
   <div class="position-relative w-100 min-w-100 max-w-100 d-flex flex-row align-items-center overflow-hidden view-messages-container">
     <!---- Components ---->
     <widget-serverbar/>
-
-    <!---- Navigation Manager ---->
-    <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-self-start disable-selection overflow-hidden navigation-container">
-      <div class="navigation-header">{{navigationHeader}}</div>
-      <div v-if="!isServerSelected" class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller contacts-container">
-        <widget-contactbar v-for="(personalGroup, groupUID) in personalGroups" :key="groupUID" :isSelected="isGroupSelected(groupUID)" :contactUID="personalGroup.participantUID" contactAvatar="https://hi-static.z-dn.net/files/d60/c746efb8807770ea7ad2af25ee7ed2ab.jpg" @click.native="onClientSelectGroup(groupUID)"/>
-      </div>
-    </div>
+    <widget-groupbar/>
 
     <!---- Navigation Renderer ---->
     <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column view-container">
