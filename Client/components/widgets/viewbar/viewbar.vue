97fb74f0595449c8e65b1102e4c920e6e3bff889
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-19 23:11:53 +0300

    Updated name class

--- Client/components/widgets/viewbar/viewbar.vue
+++ Client/components/widgets/viewbar/viewbar.vue
@@ -17,8 +17,7 @@
     <b-icon class="menu-headerIcon" icon="arrow-return-right"/>
     <div class="menu-header">{{this.$store.state.app.menu}}</div>
     <div class="w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-end viewbar-container">
-      <div class="client-name">{{getClientName()}}</div>
-      <span class="d-flex flex-row align-items-center justify-content-center client-subscriber" isSelected="true"><b-icon icon="patch-check"/></span>
+      <div class="d-flex flex-row align-items-center justify-content-start client-name"><span class="client-subscriber"><b-icon icon="patch-check"/></span>{{getClientName()}}</div>
       <span class="d-flex flex-row align-items-center justify-content-center" isSelected="true"><b-icon icon="plus"/>100</span>
       <span class="d-flex flex-row align-items-center justify-content-center option-container" @click.prevent="onClientLogout"><b-icon icon="box-arrow-in-right"/></span>
     </div>
