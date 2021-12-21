719780725cd519f8e732db337ec2f4a6bccee60c
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-21 19:54:32 +0300

    Update app.js

--- Client/store/app.js
+++ Client/store/app.js
@@ -31,7 +31,7 @@ export const mutations = {
   },
 
   setServerGroupSelection(state, group, channel) {
-    state.serverGroup.group = ($nuxt.$store.state.groups.server.userGroups[group] && group) || false
+    state.serverGroup.group = (group && $nuxt.$store.state.groups.server.userGroups[group] && group) || false
     state.serverGroup.channel = (state.serverGroup.group && channel && $nuxt.$store.state.groups.server.userGroups[(state.serverGroup.group)].channels[channel] && channel) || Object.keys($nuxt.$store.state.groups.server.userGroups)[(state.serverGroup.group)].channels[0] || false
   }
 }
