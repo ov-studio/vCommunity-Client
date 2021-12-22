cc83f829eb346c4e27b3f50eeae0ba28be028801
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-23 00:24:39 +0300

    Fnalized

--- Client/components/widgets/server/channelbar/channelbar.vue
+++ Client/components/widgets/server/channelbar/channelbar.vue
@@ -18,13 +18,7 @@
     <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller channelbar-container">
       <div class="d-flex flex-column align-items-center justify-content-start channel-container">
         <div class="w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-start container-header"><div class="w-100">CHANNELS</div><b-icon class="channel-adder" icon="plus" @click.prevent="onCreateChannel()"/></div>
-        <div v-for="(serverChannel, channelUID) in serverChannels" :key="channelUID"  class="d-flex flex-row align-items-center justify-content-start channel" :isSelected="isChannelSelected(channelUID)" @click.prevent="onClientSelectChannel(channelUID)"><div>{{serverChannel.name}}</div></div>
-        <!--:isSelected="isChannelSelected(groupUID)" div class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 1</div></!--:isSelected=>
-        <div class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 2</div></div>
-        <div class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 3</div></div>
-        <div class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 4</div></div>
-        <div class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 5</div></div>
-        <div--- class="d-flex flex-row align-items-center justify-content-start channel"><div>Channel 6</div></div--->
+        <div v-for="(serverChannel, channelUID) in serverChannels" :key="channelUID"  class="w-100 d-flex flex-row align-items-center justify-content-start channel" :isSelected="isChannelSelected(channelUID)" @click.prevent="onClientSelectChannel(channelUID)"><div>{{serverChannel.name}}</div></div>
       </div>
     </div>
   </div>
