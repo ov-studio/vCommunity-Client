ff83556aa8d2f318d1ae07ff2f8e930b451a5c76
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-22 03:20:13 +0300

    Updated

--- Client/components/widgets/server/channelbar/channelbar.vue
+++ Client/components/widgets/server/channelbar/channelbar.vue
@@ -14,8 +14,8 @@
 
 <template>
   <div class="position-relative w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-self-start align-items-center justify-content-start overflow-hidden disable-selection widget-server-channelbar-container">
-    <div class="w-100 min-w-100 max-w-100 groupbar-header">{{containerHeader}}</div>
-    <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller contacts-container">
+    <div class="w-100 min-w-100 max-w-100 channelbar-header">{{containerHeader}}</div>
+    <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller channelbar-container">
       <div class="d-flex flex-column align-items-center justify-content-start channel-container">
         <div class="d-flex flex-row align-items-center justify-content-start container-header"><div class="w-100">CHANNELS</div><b-icon class="channel-adder" icon="plus" @click.prevent="onCreateChannel()"/></div>
         <div v-for="(serverChannel, channelUID) in serverChannels" :key="channelUID"  class="d-flex flex-row align-items-center justify-content-start channel" :isSelected="isChannelSelected(channelUID)" @click.prevent="onClientSelectChannel(channelUID)"><div>{{serverChannel.name}}</div></div>
