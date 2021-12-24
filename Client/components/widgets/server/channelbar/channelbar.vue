cf06d0f37ed1061d3c3811e8cae9532ea93e1e41
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-24 04:16:15 +0300

    Updated

--- Client/components/widgets/server/channelbar/channelbar.vue
+++ Client/components/widgets/server/channelbar/channelbar.vue
@@ -14,10 +14,16 @@
 
 <template>
   <div class="position-relative w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 d-flex flex-column align-self-start align-items-center justify-content-start overflow-hidden disable-selection widget-server-channelbar-container">
-    <div class="w-100 min-w-100 max-w-100 channelbar-header">{{containerHeader}}</div>
+    <div class="w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-center overflow-hidden channelbar-header">
+      <div class="w-100">{{containerHeader}}</div>
+      <div class="server-option"><b-icon icon="grip-vertical"/></div>
+    </div>
     <div class="w-100 h-100 min-w-100 min-h-100 max-w-100 max-h-100 hide-scroller channelbar-container">
       <div class="d-flex flex-column align-items-center justify-content-start channel-container">
-        <div class="w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-start container-header"><div class="w-100">CHANNELS</div><b-icon class="channel-adder" icon="plus" @click.prevent="onChannelCreatorProcess()"/></div>
+        <div class="w-100 min-w-100 max-w-100 d-flex flex-row align-items-center justify-content-start container-header">
+          <div class="w-100">CHANNELS</div>
+          <b-icon class="channel-adder" icon="plus" @click.prevent="onChannelCreatorProcess()"/>
+        </div>
         <div v-for="(serverChannel, channelUID) in serverChannels" :key="channelUID"  class="w-100 d-flex flex-row align-items-center justify-content-start channel" :isSelected="isChannelSelected(channelUID)" @click.prevent="onClientSelectChannel(channelUID)"><div>{{serverChannel.name}}</div></div>
       </div>
     </div>
