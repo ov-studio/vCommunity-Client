c44ca2907868ec5fd1c4f160043d5883b17afc16
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-24 23:28:43 +0300

    Update script.js

--- Client/components/elements/dropdown/script.js
+++ Client/components/elements/dropdown/script.js
@@ -1,5 +1,25 @@
 export default {
   props: ["dropdownHeader"],
 
-  methods: {}
+  data() {
+    return {
+      isVisible: false
+    }
+  },
+
+  methods: {
+    createWidget() {
+      if (this.isVisible) return false
+
+      this.isVisible = true
+      this.$emit("created")
+    },
+
+    destroyWidget() {
+      if (!this.isVisible) return false
+
+      this.isVisible = false
+      this.$emit("destroyed")
+    }
+  }
 }
